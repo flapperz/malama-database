74da72d2ad8342c1c28f776b8638c75f968ee9cb
kanjeranian <oilscius@gmail.com>
2019-11-21 21:06:21 +0700

    now can edit cafeDog

--- public/public/script.js
+++ public/public/script.js
@@ -1,6 +1,7 @@
 //--------variable----------------------
 
 var max_id = 0;
+var edit_id = 0;
 
 //--------Form Managrment---------------
 
@@ -92,15 +93,70 @@ function findCafeDog(id) {
   }
 }
 
+function editCafeDog() {
+  var error = '';
+  const ID = edit_id;
+  var NAME = $("[id='edit_cafeDog_dog_name']").val();
+  var BREED = $("[id='edit_cafeDog_breed']").val();
+  var BIRTHDATE = $("[id='edit_cafeDog_date_of_birth']").val();
+  var WEIGHT = $("[id='edit_cafeDog_weight']").val();
+  var COMPANY = $("[id='edit_cafeDog_sourcing_company']").val();
+  var PRICE = $("[id='edit_cafeDog_brought_price']").val();
+  var LAST_CHECK = $("[id='edit_cafeDog_last_checkup_date']").val();
+  var LAST_BATH = $("[id='edit_cafeDog_last_bath_date']").val();
+  var HEALTH = $("[id='edit_cafeDog_health_status']").val();
+  var SHOWTIME = $("[id='edit_cafeDog_show-time']").val();
+
+  if (
+    NAME == '' ||
+    BREED == '' ||
+    BIRTHDATE == '' ||
+    WEIGHT == '' ||
+    COMPANY == '' ||
+    PRICE == '' ||
+    LAST_CHECK == '' ||
+    LAST_BATH == '' ||
+    HEALTH == null ||
+    SHOWTIME == null
+  ) {
+    error = addError(error, 'Fields required');
+  }
+
+  if (error != '') {
+    window.alert(error);
+    return;
+  } else {
+    axios
+      .put('http://localhost:5000/cafedog/' + ID, {
+        dog_id: ID,
+        dog_name: NAME,
+        breed: BREED,
+        date_of_birth: BIRTHDATE,
+        weight: WEIGHT,
+        last_checkup_date: LAST_CHECK,
+        last_bath_date: LAST_BATH,
+        health_status: HEALTH,
+        sourcing_company: COMPANY,
+        brought_price: PRICE,
+        showtime: SHOWTIME
+      })
+      .then(function(response) {
+        console.log(response);
+        hideFormEditCafeDog();
+        getCafeDog();
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+}
+
 function edit(id) {
   showFormEditCafeDog();
-  //ถ้าขยันก็ทำค่า default ไป ถ้า ขก.ก็ทำแบบที่เคยทำอะ55555
-  // document.getElementById('dog_name').value = 'new value';
-  // $("[name='dog_name']").val('oil');
-  // console.log('edit' + id);
+  edit_id = id;
+
   var find = findCafeDog(id);
   find.then(dog => {
-    console.log(dog.last_checkup_date);
     $("[name='dog_name']").val(dog.dog_name);
     $("[name='breed']").val(dog.breed);
     $("[name='date_of_birth']").val(dog.date_of_birth);
@@ -112,28 +168,6 @@ function edit(id) {
     $("[name='health_status']").val(dog.health_status);
     $("[name='show-time']").val(dog.showtime);
   });
-  // console.log('end');
-  // console.log(dog.dog_name);
-  // try {
-  //   axios.get('http://localhost:5000/cafedog').then(response => {
-  //     console.log(response);
-
-  //     for (let i = 0; i < response.data.length; i++) {
-  //       if (i == ID) {
-  //       }
-  //       const ID = response.data[i].dog_id;
-  //       const NAME = response.data[i].dog_name;
-  //       const BREED = response.data[i].breed;
-  //       const BIRTHDATE = response.data[i].date_of_birth.slice(0, 10);
-  //       const WEIGHT = response.data[i].weight;
-  //     }
-  //     html += '</table>';
-  //     document.getElementById('dog-display').innerHTML = html;
-  //   });
-  // } catch (error) {
-  //   console.log('failed');
-  //   console.error(error);
-  // }
 }
 
 function delCafeDog(id) {
--- server/Server.js
+++ server/Server.js
@@ -4,10 +4,17 @@ const express = require('express'),
 port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Methods', 'POST');
+  // res.header('Access-Control-Allow-Methods', 'DELETE');
+  // res.header('Access-Control-Allow-Headers', 'accept, content-type');
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST');
-  res.header('Access-Control-Allow-Methods', 'DELETE');
-  res.header('Access-Control-Allow-Headers', 'accept, content-type');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
   next();
 });
 
